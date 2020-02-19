229d1f1d1a476d81794f9c42914a07aea5b50143
softwarica <softwarica@RAJs-Mac-mini.local>
2020-02-19 09:02:09 +0545

    Hello World

---
+++ iOSFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSFirstApp
+//
+//  Created by Suresh Chaudhari on 2/19/20.
+//  Copyright © 2020 codebrust. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! Again")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
